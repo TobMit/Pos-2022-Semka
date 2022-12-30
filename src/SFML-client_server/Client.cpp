736135381a54b6a7463577d3d5aa84f13811b8b5
TobMit <80719513+tobmit@users.noreply.github.com>
2022-12-30 14:54:56 +0100

    testing wrapper class for sfml networking half done - can't build

--- src/SFML-client_server/Client.cpp
+++ src/SFML-client_server/Client.cpp
@@ -1,93 +1,28 @@
-#include <SFML/Network.hpp>
-#include <istream>
+#include "Sockets/Client.h"
 #include <iostream>
-#include <thread>
-#include <mutex>
-#include <unistd.h>
-#include <sys/select.h>
-#include <fcntl.h>
-#include <condition_variable>
-#include "../Constants.h"
-
-bool isEnd(std::mutex *mut, bool *pEnd) {
-    bool end;
-    {
-        std::unique_lock<std::mutex> lock(*mut);
-        end = *pEnd;
-    }
-    return end;
-}
-
-void setEnd(std::mutex *mut, bool *pEnd) {
-    std::unique_lock<std::mutex> lock(*mut);
-    *pEnd = true;
-}
-
-void writeToBuffer(std::mutex *mut, sf::TcpSocket *socket, bool *end)
-{
-    char buffer[constants::BUFF_SIZE + 1];
-    buffer[constants::BUFF_SIZE] = '\0';
-
-    fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL, 0) | O_NONBLOCK);
-    fd_set inputs;
-    FD_ZERO(&inputs);
-    struct timeval tv;
-    tv.tv_usec = 0;
-    while (!isEnd(mut, end)) {
-        int position = -1;
-        tv.tv_sec = 1;
-        FD_SET(STDIN_FILENO, &inputs);
-        select(STDIN_FILENO + 1, &inputs, NULL, NULL, &tv);
-
-        if (FD_ISSET(STDIN_FILENO, &inputs)) {
-            char *testStart = buffer;
-
-            while (fgets(testStart, constants::BUFF_SIZE-1, stdin)!= nullptr) {
-                std::string inputFromConsole = testStart;
-                position = inputFromConsole.find(":end",0);
-                //std::cout << testStart << std::endl;
-                {
-                    std::unique_lock<std::mutex> lock(*mut);
-                    sf::Packet packet;
-                    packet << inputFromConsole;
-                    if (socket->send(packet) != sf::Socket::Done) {
-                        setEnd(mut, end);
-                    }
-                }
-
-            }
-        }
-        if (position != -1) {
-            setEnd(mut, end);
-        }
-    }
-    fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL, 0) & ~O_NONBLOCK);
-
-}
 
 int main() {
-    std::cout << "Client is started!" << std::endl;
-    sf::TcpSocket socket;
-    sf::Socket::Status status = socket.connect("localhost", constants::PORT);
-    if (status != sf::Socket::Done)
+    std::cout << "Client started!" << std::endl;
+    Client client;
+    if (client.socketConnect("localhost", constants::PORT))
     {
         std::cerr << "Error connection!" << std::endl;
         return EXIT_FAILURE;
     }
     std::cout << "Client is connected to the server" << std::endl;
+
     bool end = false;
-    //std::list<std::string> buffer;
     std::mutex mut;
     std::condition_variable writeToBuff, readFromBuff;
-    std::thread writer(writeToBuffer, &mut, &socket, &end);
+    std::thread writer(client.writeToBuffer, &mut, &end);
 
-    while (!isEnd(&mut, &end)) {
+    while (!client.isEnd(&mut, &end)) {
         std::string fromServer;
         sf::Packet packet;
         int position = -1;
 
-        if (socket.receive(packet) != sf::Socket::Done) {
-            setEnd(&mut, &end);
+        if (client.socketReceive(&packet)) {
+            client.setEnd(&mut, &end);
         }
 
         if (packet >> fromServer){
@@ -98,11 +33,11 @@ int main() {
         }
 
         if (position != -1) {
-            setEnd(&mut, &end);
+            client.setEnd(&mut, &end);
         }
     }
     writer.join();
-    socket.disconnect();
+    client.socketDisconect();
     std::cout << "Connections is aborted " << std::endl;
     return EXIT_SUCCESS;
 
